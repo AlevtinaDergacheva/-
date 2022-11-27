25e6e7e7de24dacd68aba9245e284e49e4947ec2
AlevtinaDergacheva <112809724+AlevtinaDergacheva@users.noreply.github.com>
2022-11-27 23:34:48 +0300

    Delete Python/practic1 directory

--- Python/practic1/task2.py
+++
@@ -1,7 +0,0 @@
-# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-
-for X in 0, 1:
-    for Y in 0, 1:
-        for Z in 0, 1:
-            print(f"X={X}, Y={Y}, Z={Z}")
-            print(not (X or Y or Z) == (not X and not Y and not Z))
